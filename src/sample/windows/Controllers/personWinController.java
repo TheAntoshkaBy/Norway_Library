abcda4ac418810deda38101f24c60b03b5373ec9
TheAntoshka <y.menya.ee.nety@gmail.com>
2018-12-04 20:40:44 +0300

    init

--- src/sample/windows/Controllers/personWinController.java
+++ src/sample/windows/Controllers/personWinController.java
@@ -1,7 +1,14 @@
 package sample.windows.Controllers;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 import sample.Main;
+
+import java.io.IOException;
 
 public class personWinController {
 
@@ -20,10 +27,30 @@ public class personWinController {
     @FXML
     void initialize()
     {
-       prevWin.setOnAction(event -> {
+        prevWin.setOnAction(event -> {
             prevWin.getScene().getWindow().hide();
 
-       });
+        });
+
+        registration.setOnAction(event -> {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/sample/windows/FXML's/registrationWin.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Parent root = loader.getRoot();
+
+            Stage stage = new Stage();
+            Scene scene = new Scene(root);
+            stage.getIcons().add(new Image(getClass().getResourceAsStream("/sample/Images/2.png")));
+            scene.getStylesheets().add(getClass().getResource("/sample/cssFiles/registrationWin.css").toExternalForm());
+            stage.setScene(scene);
+            stage.showAndWait();
+
+
+        });
     }
 
 }
